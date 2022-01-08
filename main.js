2d6ece12042ae8d29dbaf9c76b62cedb1702b432
Kavya-349 <83696676+Kavya-349@users.noreply.github.com>
2022-01-08 11:57:18 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -6,22 +6,16 @@ var leftWristY = 0;
 var rightWristX = 0;
 var rightWristY = 0;
 
+var scoreLeftWrist = 0;
+var scoreRightWrist  = 0;
+
 function preload() {
-    song_names[0].loadSound("Aage Peeche.mp3");
-    song_names[1].loadSound("Ankhiyon Se Goli Marre.mp3");
-    song_names[2].loadSound("Are Yaaro Mere Pyaro.mp3");
-    song_names[3].loadSound("Chahe Koi Mujhe Junglee.mp3");
-    song_names[4].loadSound("Dafli Waale Dafli Bajaa.mp3");
-    song_names[5].loadSound("Ek Ladki Ko Dekha.mp3");
-    song_names[6].loadSound("Jai Jai Shiv Shankar.mp3");
-    song_names[7].loadSound("Kora Kagaz Tha Yeh Man Mera.mp3");
-    song_names[8].loadSound("Lag Ja Gale Se Phir.mp3");
-    song_names[9].loadSound("Le Gayi.mp3");
-    song_names[10].loadSound("Likhe Jo Khat Tujhe.mp3");
-    song_names[11].loadSound("Pehla Nasha.mp3");
-    song_names[12].loadSound("Tayyab Ali Pyar Ka Dushman.mp3");
-    song_names[13].loadSound("Uden Jab Jab Zulfen Teri.mp3");
-    song_names[14].loadSound("Yaha Ke Hum Sikandar.mp3");
+    song_names[0] = loadSound("Aage Peeche.mp3");
+    song_names[1] = loadSound("Are Yaaro Mere Pyaro.mp3");
+    song_names[2] = loadSound("Chahe Koi Mujhe Junglee.mp3");
+    /*song1= loadSound("Aage Peeche.mp3");
+    song2= loadSound("Are Yaaro Mere Pyaro.mp3");
+    song3= loadSound("Chahe Koi Mujhe Junglee.mp3");*/
 }
 
 function setup() {
@@ -41,6 +35,8 @@ function modalLoaded() {
 
 function gotPoses(answers) {
     if(answers.length > 0) {
+        console.log(answers);
+
         leftWristX = answers[0].pose.leftWrist.x;
         leftWristY = answers[0].pose.leftWrist.y;
 
@@ -49,9 +45,61 @@ function gotPoses(answers) {
 
         console.log("Left Wrist X - " + leftWristX + " Left Wrist Y - " + leftWristY 
         + " Right Wrist X - " + rightWristX + " Right Wrist Y - " + rightWristY);
+
+        scoreLeftWrist = answers[0].pose.keypoints[9].score;
+        scoreRightWrist = answers[0].pose.keypoints[10].score;
+        console.log("Left Wrist Score - " + scoreLeftWrist + " Right Wrist Score - " + scoreRightWrist)
     }
 }
 
 function draw() {
     image(video,0,0,700,600);
+
+    song1_status = song_names[0].isPlaying();
+    song2_status = song_names[1].isPlaying();
+    song3_status = song_names[2].isPlaying();
+
+
+    fill("#FF0000");
+    stroke("#FF0000");
+
+    if(scoreLeftWrist > 0.2) {
+
+        circle(leftWristX,leftWristY,15);
+        song1.stop();
+        if((song1_status==false)&&(song3_status==true))
+        {
+            song_names[2].stop();
+            song_names[1].play();
+            document.getElementById("song").innerHTML = "Playing: Are Yaaro Mere Pyaro";
+        } else if((song2_status == true) && (song3_status == false)){
+            song_names[1].stop();
+            song_names[2].play();
+            document.getElementById("song").innerHTML = "Playing:Chahe Koi Mujhe Junglee";
+            }      
+    }
+
+    if(scoreRightWrist > 0.2) {
+
+        circle(rightWristX,rightWristY,15);
+        song3.stop();
+        if((song3_status==false)&& (song1_status==true)){
+            song_names[0].stop();
+            song_names[1].play();
+            document.getElementById("song").innerHTML="Playing: Aage Peeche";
+        }
+        else if((song3_status==false)&&(song2_status==true)){
+            song_names[1].stop();
+            song_names[0].play();
+            document.getElementById("song").innerHTML="Playing: Are Yaaro Mere Pyaro";
+        
+        }
+       
+    }
+}
+
+function play() {
+    song_names[0].play();
+    song_names[0].setVolume(1);
+    song_names[0].rate(1);
 }
